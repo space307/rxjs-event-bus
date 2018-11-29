c95c660c8ae66b41511e009c4bfd23e3684cf88b
Sergey Kulakov <s.kulakov@space307.com>
2018-11-29 22:10:38 +0300

    emit event in special stream when new stream has been created

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import type {
   StreamType,
 } from './types.js'
 
+export const NEW_BUS_STREAM_CREATED = 'event-bus/new-stream';
 
 export default class Bus<INC: { [string]: * }> {
   _subjectsEmitter: ReplaySubject<*>
@@ -20,10 +21,6 @@ export default class Bus<INC: { [string]: * }> {
   constructor(historySettings?: HistorySettingsType<INC>) {
     this._subjectsEmitter = new ReplaySubject()
     this._historySettings = historySettings || new Map()
-
-    if (historySettings) {
-      this._initReplaySubjects(historySettings)
-    }
   }
 
   select<T: EventType<INC>>(type: T, sliceCount?: number): Observable<BusEvent<INC, T>> {
@@ -83,16 +80,24 @@ export default class Bus<INC: { [string]: * }> {
     }
   }
 
-  _initReplaySubjects(historySettings: HistorySettingsType<INC>) {
-    for (var [type, value] of historySettings.entries()) {
-      this._addNewStream(type, new ReplaySubject(value))
-    }
-  }
-
-  _addNewStream<T: EventType<INC>>(type: T, stream: StreamType<BusEvent<INC, T>>) {
+  _pushNewStream<T: EventType<INC>>(type: T, stream: StreamType<BusEvent<INC, T>>) {
     // TODO: flow error
     // $FlowIgnore i don't know
     this._streams.set(type, stream)
     this._updateMainStream(type)
+    if(type !== NEW_BUS_STREAM_CREATED) {
+      this.emit({
+        type: NEW_BUS_STREAM_CREATED,
+        payload: type,
+      })
+    }
+  }
+
+  _addNewStream<T: EventType<INC>>(type: T) {
+    const historyLength = this._historySettings.get(type)
+    let stream = historyLength
+      ? new ReplaySubject(historyLength)
+      : new Subject();
+    this._pushNewStream(type, stream)
   }
 }
